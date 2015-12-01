d26f62321eb8c39642c9708c4bd274cf68605f48
David Rubert <david.rubert@gmail.com>
2015-12-01 18:41:43 +0100

    docker

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,8 +2,50 @@
 
 import * as mongoose from "mongoose";
 
-module.exports = mongoose.model("User", new mongoose.Schema({
-	username: String,
-	password: String,
-	admin: Boolean
-}));
+interface IUser extends mongoose.Document {
+    username: string;
+    password: string;
+    admin: boolean;
+    updated: Date;
+}
+
+var _schema: mongoose.Schema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true
+    },
+    password: String,
+    admin: Boolean,
+    updated: {
+        type: Date,
+        default: Date.now
+    }
+}).pre('save', function(next) {
+    this.updated = new Date();
+    next();
+});
+
+
+var _model = mongoose.model < IUser > ('User', _schema);
+
+class User {
+
+    private _document: IUser;
+    
+    constructor(document: IUser) {
+        this._document = document;
+    }
+    
+    static findByUsername(username: string): Promise <IUser> {
+        return new Promise <IUser>((resolve, reject) => {
+            _model.findOne({ username: username}).exec().then(user => {
+                resolve(user);
+                console.log(user);
+            }).reject((err) => {
+                reject(err);
+            })
+        })
+    }
+}
+
+export = User;
--- src/app/server.ts
+++ src/app/server.ts
@@ -10,31 +10,49 @@ interface IExpressConfig {
 	secret: string;
 	port: number;
 	environment: string;
+	logger: {
+		format: string
+	}
 }
 
+interface ILoggerConfig {
+	stream?: TextStreamWriter,
+	format: string
+}
 
 class App {
 	app:express.Express;
 	db: DocumentDB;
+	
+	// use morgan to log requests to the console
+	configureLogger(loggerConfig?:ILoggerConfig) {
+		this.app.use(morgan(loggerConfig.format));
+	}
+	
+	// use body parser so we can get info from POST and/or URL parameters
+	configureBodyParser() {
+		this.app.use(bodyParser.urlencoded({ extended: false }));
+		this.app.use(bodyParser.json());
+	}
+	
+	configureRoutes() {
+		var routes = require("./routes");
+		this.app.use("/", routes);
+	}
 	
 	constructor(public config:IExpressConfig) {
 		this.app = express(); 		
 		this.db = new DocumentDB(expressConfig.database);
 		this.app.set("superSecret", expressConfig.secret);
 		
-		// use body parser so we can get info from POST and/or URL parameters
-		this.app.use(bodyParser.urlencoded({ extended: false }));
-		this.app.use(bodyParser.json());
-		
-		// use morgan to log requests to the console
-		this.app.use(morgan(this.config.environment));
-		
-		var routes = require("./routes");
-		this.app.use("/", routes);
+		this.configureLogger(this.config.logger);
+		this.configureBodyParser();
+		this.configureRoutes();
 	}
 	
 	start() {
 		this.app.listen(this.config.port);
+		console.log("Running! Listening in port " + this.config.port);
 	}
 }
 
